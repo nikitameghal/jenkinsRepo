13201b234a50e1620a8960c6f8a0f62acc527ac7
nikitameghal <niki.1.meghal@gmail.com>
2022-08-16 15:21:40 +0530

    jenkins commited

---
+++ src/test/java/com/DummyJenkins/createCampaingn/CreateCapaignTest.java
@@ -0,0 +1,33 @@
+package com.DummyJenkins.createCampaingn;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateCapaignTest {
+
+	@Test
+	public void createcampTest() throws Throwable
+	{
+		WebDriver driver=new ChromeDriver();
+		
+		FileInputStream fis = new FileInputStream("./CommonProperties.properties");
+		Properties pobj = new Properties();
+		pobj.load(fis);
+		System.out.println("login to appplication");
+		String url= pobj.getProperty("Url");
+		driver.get(url);
+		
+		System.out.println("url");
+		System.out.println("welcome to jenkins");
+		
+		driver.close();
+		
+	}
+	
+}
